6dd2f2bf75eb146db940a096b8815f9ff0d95097
Arnaud Amzallag <arnaud.amzallag@gmail.com>
2022-02-24 23:12:54 -0500

    Added constructor and state

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+        val: null,
+        valeur: this.props.valeur,
+    }
+}
+
     render() {
       return (
-        <button className="square">
-          {this.props.value + 1}
+        <button
+          className="square"
+          onClick={() => this.setState({val: this.props.valeur+1})}
+        >
+          {this.state.val}
         </button>
       );
     }
@@ -25,7 +36,7 @@ class Square extends React.Component {
 
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square value={i} />;
+      return <Square valeur={i} />;
     }
     // renderSep() {
     //     return <div className="sep"/>;
